6303d75c5eb24b7cf9e7d19972ae15a3311fc5ae
cjh1990 <1710196486@163.com>
2017-09-16 15:52:41 +0800

    hello文件

---
+++ hello.c
@@ -0,0 +1,19 @@
+/*************************************************************************
+    > File Name: hello.c
+    > Author: Maple
+    > mail: 772866136@qq.com 
+    > Created Time: 2017年09月16日 星期六 15时51分42秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<sys/types.h>
+int main(int argc,char *argv[])
+{
+   
+	printf("hahahahah\n");
+    return 0;
+
+}
